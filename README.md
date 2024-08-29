39845854eb538d19a1cb8c4a16371ff21389036a
joeban <joeban@twbeex.com>
2024-08-29 14:19:46 +0800

    Update: README.md and Home page

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # nodejs-shop-mysql
-nodejs + react + mySql + sequelize orm
+這是一個練習 Node.js + MySQL + Sequelize ORM + nextjs 的專案
+---
+
+## 專案架構
+
+- 使用 Node.js 當 Server
+- MySQL + Sequelize ORM 在 Node.js 內處理 DB
+- Next.js 渲染畫面
+
+## 備註
+
+- 目前這篇的權限還沒實作，User and Cart 是寫死，固定使用同一個 User + 同一個 Cart 來實現，DB 處理資料的練習
+- 參考課程: [Node.js - The Complete Guide (incl. MVC, REST APIs, GraphQL)](https://www.udemy.com/course/nodejs-the-complete-guide/?couponCode=SKILLS4SALEA)
--- frontend-app/src/app/page.tsx
+++ frontend-app/src/app/page.tsx
@@ -1,3 +1,31 @@
 export default function Home() {
-  return <div className="w-full h-full">Home page</div>;
+  return (
+    <div className="w-full h-full flex flex-col gap-4">
+      <h1 className="text-4xl font-bold">Home page</h1>
+      <p>這是一個練習 nodejs + mySql + sequlize ORM 的專案</p>
+      <h2 className="text-xl font-bold">專案架構：</h2>
+      <ul className="list-disc pl-5">
+        <li>使用 nodejs 當 server</li>
+        <li>MySql + sequlize ORM 在 nodejs 內處理 DB</li>
+        <li>nextjs 渲染畫面</li>
+      </ul>
+      <hr />
+      <h2 className="text-xl font-bold">備註：</h2>
+      <ul className="list-disc pl-5">
+        <li>
+          目前這篇的權限還沒實作，User and cart 是寫死，固定使用同一個 user +
+          同一個 cart 來實現，DB 處理資料的練習
+        </li>
+        <li>
+          ref 課程：
+          <a
+            className="text-blue-400 hover:text-blue-500"
+            href="https://www.udemy.com/course/nodejs-the-complete-guide/?couponCode=SKILLS4SALEA"
+          >
+            https://www.udemy.com/course/nodejs-the-complete-guide/?couponCode=SKILLS4SALEA
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 }
