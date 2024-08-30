c88d1a5f931973b13e16af4af1b9d2b1920bda61
joeban <joeban@twbeex.com>
2024-08-30 10:55:38 +0800

    fix: id => uuid

--- node-api/models/cart.js
+++ node-api/models/cart.js
@@ -3,8 +3,8 @@ const sequelize = require("../util/database");
 
 const Cart = sequelize.define("cart", {
   id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
+    type: Sequelize.DataTypes.UUID,
+    defaultValue: Sequelize.DataTypes.UUIDV4, // 使用 UUIDV4 作為默認值
     allowNull: false,
     primaryKey: true,
   },
